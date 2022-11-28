5ac13d0899002eccec94b8bc04b3924ac60187cf
Matthew Jamison <mjamison2802@gmail.com>
2022-11-28 03:52:06 -0600

    Fixed hover state on Table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = Book.order(sort_column + ' ' + sort_direction)
+    @books = Book.all.order(sort_column + ' ' + sort_direction)
   end
 
   # GET /books/1 or /books/1.json
